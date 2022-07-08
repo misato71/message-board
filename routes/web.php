3ace656e8700d3a000b3cc3ae8dc0b4db93b265c
misato <misato71h@gmail.com>
2022-07-08 02:44:30 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+
+Route::resource('messages', 'MessagesController');
+
+
+
+
+// // CRUD
+// //メッセージ個別詳細のページ表示
+// Route::get('messages/{id}', 'MessagesController@show');
+// //メッセージの新規登録を処理
+// Route::post('messages', 'messagesController@store');
+// //メッセージ更新処理
+// Router::put('messages/{id}', 'MessagesController@update');
+// //メッセージを削除
+// Route::delete('messages/{id}', 'MessagesController@destroy');   
+
+// //index: showの補助ページ
+// Route::get('messages', 'MessagesController@index')->name('messages.index');
+// //create: 新規作成のフォームページ
+// Route::get('messages/create', 'MessagesController@create')->name('messages.create');
+// //edit: 更新用のフォームページ
+// Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
