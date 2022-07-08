9f83f4e33f9c4748b6e81c6f71ee5852f134bf4b
misato <misato71h@gmail.com>
2022-07-08 10:50:48 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,10 @@
         {{-- ナビゲーションバー --}}
         @include('commons.navbar')
         
-        <div class="content">
+        <div class="container">
+            {{--  エラーメッセージ  --}}
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
         
